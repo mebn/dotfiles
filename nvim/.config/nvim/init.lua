74c90d6fc8fc07e55f84068fd51073aec425c688
Marcus Nilszén <nilszen.marcus@gmail.com>
2024-07-22 09:48:09 +0200

    update: lots of changes

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -47,7 +47,26 @@ require("lazy").setup({
     "nvim-tree/nvim-web-devicons",
     "nvim-tree/nvim-tree.lua",
 
+    -- rust
     "simrat39/rust-tools.nvim",
+
+    -- go
+    {
+        "ray-x/go.nvim",
+        dependencies = {  -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("go").setup()
+            end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    },
+
+
     "lewis6991/gitsigns.nvim",
     "itchyny/vim-gitbranch",
     "tpope/vim-commentary", -- (un)comment with gcc, gc, gcip etc
@@ -60,6 +79,18 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.commentstring = "// %s"
     end
 })
+
+-- go
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
+
+require('go').setup()
 
 -- all other plugin settings
 require("options")
